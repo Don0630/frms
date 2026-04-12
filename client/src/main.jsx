61712897ead7c7cc7da6db17b237f2a1bc6631a3
Don0630 <donelraza0630@gmail.com>
2026-04-12 15:29:49 +0800

    updated all page logic transaction

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,17 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { AuthProvider } from "./context/AuthContext.jsx"
-import { UserProvider } from "./context/UserContext.jsx"
-import { StaffProvider } from "./context/StaffContext.jsx"
-import { ProgramProvider } from "./context/ProgramContext.jsx"
-import { SubsidyProvider } from "./context/SubsidyContext.jsx"
-import { SubsidyDetailsProvider } from "./context/SubsidyDetailsContext.jsx"
-import { FarmerProvider } from "./context/FarmerContext.jsx"
-import { CropProvider } from "./context/CropContext.jsx"
-import { LivestockProvider } from "./context/LivestockContext.jsx"
-import { MonitoringProvider } from "./context/MonitoringContext.jsx"
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
+import { AuthProvider } from "./context/AuthContext.jsx";
+import { UserProvider } from "./context/UserContext.jsx";
+import { StaffProvider } from "./context/StaffContext.jsx";
+import { ProgramProvider } from "./context/ProgramContext.jsx";
+import { SubsidyProvider } from "./context/SubsidyContext.jsx";
+import { SubsidyDetailsProvider } from "./context/SubsidyDetailsContext.jsx";
+import { FarmerProvider } from "./context/FarmerContext.jsx";
+import { CropProvider } from "./context/CropContext.jsx";
+import { LivestockProvider } from "./context/LivestockContext.jsx";
+import { MonitoringProvider } from "./context/MonitoringContext.jsx";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
